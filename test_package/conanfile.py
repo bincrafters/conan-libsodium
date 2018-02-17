e207eba92b3d61e01838ca78b49c28d5048d58ba
SSE4 <tomskside@gmail.com>
2018-02-17 19:02:50 +0700

    - test all 4 possible ways to use libsodium

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -10,16 +10,19 @@ class TestPackageConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+        for variant in ['CONAN', 'CONAN_TARGETS', 'FIND_PACKAGE', 'FIND_PACKAGE_TARGETS']:
+            cmake = CMake(self)
+            cmake.definitions['BUILD_VARIANT'] = variant
+            cmake.configure(build_dir=variant)
+            cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            bin_path = os.path.join("bin", "test_package")
-            if self.settings.os == "Windows":
-                self.run(bin_path)
-            elif self.settings.os == "Macos":
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
-            else:
-                self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
+        for variant in ['CONAN', 'CONAN_TARGETS', 'FIND_PACKAGE', 'FIND_PACKAGE_TARGETS']:
+            with tools.environment_append(RunEnvironment(self).vars):
+                bin_path = os.path.join(variant, "bin", "test_package")
+                if self.settings.os == "Windows":
+                    self.run(bin_path)
+                elif self.settings.os == "Macos":
+                    self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ.get('DYLD_LIBRARY_PATH', ''), bin_path))
+                else:
+                    self.run("LD_LIBRARY_PATH=%s %s" % (os.environ.get('LD_LIBRARY_PATH', ''), bin_path))
